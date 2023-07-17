a7408ffd3f4f325e725399a94cf5e4a2a2bdc5c5
Brian Raguine <brianraguine0628@gmail.com>
2023-07-18 01:20:39 +0800

    Creating Contact Form

--- src/data/RatingsCard.js
+++ src/data/RatingsCard.js
@@ -10,7 +10,9 @@ const RatingsCard = () => {
         comment="Some quick example text to build on the card title and make up the bulk of the card's content."
         imageSrc="./images/rating/harry.jpg"
         authorName="Harry Potter"
+        
       />
+      
     </div>
   );
 };
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,18 +4,19 @@ import React from "react";
 // Components ang Pages Imports
 import HeaderPages from "../components/HeaderPages";
 import Footer from "../components/Footer";
-
+import ContactUs from "../components/contact";
 // Miscellaneous Imports
 import "../styles/styles.css";
 import "../styles/themes.css";
 
 const Contact = () => {
   return (
+  
     <div className="bgColorLightTheme txtColorLightTheme">
       <HeaderPages />
 
       <h1 className="text-center">This is the Contact Page</h1>
-
+      <ContactUs/>
       <Footer />
     </div>
   );
