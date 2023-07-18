1a507aa3ece555fc61edf01e01643bb48b19136c
Charanel Gile <charanelgile@icloud.com>
2023-07-18 20:46:26 +0800

    Create Banner Image for Every Page in the Navbar
    - Deleted unused components and files in the
    project

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -3,6 +3,7 @@ import React from "react";
 
 // Components ang Pages Imports
 import HeaderPages from "../components/HeaderPages";
+import BannerImage from "../components/BannerImage";
 import Footer from "../components/Footer";
 import PricingCardsContent from "../data/PricingCardsContent";
 // Miscellaneous Imports
@@ -14,8 +15,15 @@ const Services = () => {
     <div className="bgColorLightTheme txtColorLightTheme">
       <HeaderPages />
 
-      <h1 className="text-center">This is the Services Page</h1>
-        <PricingCardsContent />
+      <BannerImage
+        imageYaxis="50%"
+        imageURL='linear-gradient(to bottom, rgba(0, 0, 0, 0.25), rgba(0, 0, 0, 0.65)),
+          url("https://images.unsplash.com/photo-1521791136064-7986c2920216?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8cXVhbGl0eSUyMHNlcnZpY2V8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=800&q=60")'
+        pageTitle="Services"
+        pageText="5 steps is all it takes to experience quality software that drives growth and efficiency all-throughout your business"
+      />
+
+      <PricingCardsContent />
       <Footer />
     </div>
   );
--- src/pages/WhoWeAre.jsx
+++ src/pages/WhoWeAre.jsx
@@ -3,6 +3,7 @@ import React from "react";
 
 // Components ang Pages Imports
 import HeaderPages from "../components/HeaderPages";
+import BannerImage from "../components/BannerImage";
 import Footer from "../components/Footer";
 
 // Miscellaneous Imports
@@ -14,7 +15,14 @@ const WhoWeAre = () => {
     <div className="bgColorLightTheme txtColorLightTheme">
       <HeaderPages />
 
-      <h1 className="text-center">About Page</h1>
+      <BannerImage
+        imageYaxis="35%"
+        imageURL='linear-gradient(to bottom, rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.4)),
+          url("https://images.unsplash.com/photo-1522202176988-66273c2fd55f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8d29ya21hdGVzfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60")'
+        pageTitle="Who We Are"
+        pageText="Get to know ABC Software Solutions more and find out what sets us
+        apart from the rest."
+      />
 
       <Footer />
     </div>
