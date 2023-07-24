ac8b35dba058059c69c44018084a673ad3e529b7
Anzarie Laguiamuda <anzariekl@gmail.com>
2023-07-24 17:57:37 +0800

    added contents on services

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -8,6 +8,7 @@ import Footer from "../components/Footer";
 // Miscellaneous Imports
 import "../styles/styles.css";
 import "../styles/themes.css";
+import ServiceContent from "../components/ServiceContent";
 
 const Services = () => {
   return (
@@ -23,62 +24,8 @@ const Services = () => {
       />
 
 
-    <div className="w-50 text-start m-5">
-        <h2>Hire Permanent and Remote Developers</h2>
-        <p>From full-time remote engineering teams to hourly contractors, work with remote dev as needed</p>
-      </div>  
-    <div className="container w-100">
-      
-      <div className="row my-3">
-        <div className="d-flex col-6">
-          <div className="me-3">
-              <img src="https://tse2.mm.bing.net/th?id=OIP.JysQbvr5i6P_fu3nA_ozKgAAAA&pid=Api&P=0&h=180" alt="Logo" width={75} />
-            </div>
-          <div>
-            <h4>Full-Stack Web App Development</h4>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius praesentium consequuntur nobis iusto! Similique magni incidunt consequuntur, natus corrupti modi?
-          </div>
-          
-        </div>
-
-        <div className="d-flex col-6">
-          <div  className="me-3">
-          <img src="https://tse2.mm.bing.net/th?id=OIP.JysQbvr5i6P_fu3nA_ozKgAAAA&pid=Api&P=0&h=180" alt="Logo" width={75} />
-            </div>
-          <div>
-            <h4>Front End Web App Development</h4>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius praesentium consequuntur nobis iusto! Similique magni incidunt consequuntur, natus corrupti modi? lorem10
-          </div>
-          
-        </div>
-      </div>
-
-      <div className="row my-3">
-        <div className="d-flex col-6">
-          <div  className="me-3">
-              <img src="https://tse2.mm.bing.net/th?id=OIP.JysQbvr5i6P_fu3nA_ozKgAAAA&pid=Api&P=0&h=180" alt="Logo" width={75} />
-            </div>
-          <div>
-            <h4>Back End Web App Development</h4>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius praesentium consequuntur nobis iusto! Similique magni incidunt consequuntur, natus corrupti modi?
-          </div>
-          
-        </div>
-
-        <div className="d-flex col-6">
-          <div className="me-3">
-          <img src="https://tse2.mm.bing.net/th?id=OIP.JysQbvr5i6P_fu3nA_ozKgAAAA&pid=Api&P=0&h=180" alt="Logo" width={75} />
-            </div>
-          <div>
-            <h4>Website Rebranding</h4>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius praesentium consequuntur nobis iusto! Similique magni incidunt consequuntur, natus corrupti modi?
-          </div>
-          
-        </div>
-      </div>
-    </div>
-
-     
+   <ServiceContent />
+ 
 
       <Footer />
     </div>
