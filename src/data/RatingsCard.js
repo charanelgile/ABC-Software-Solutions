84860f15012ba244cf650068074f1a3302577da4
Brian Raguine <brianraguine0628@gmail.com>
2023-07-30 17:39:34 +0800

    commit message

--- src/data/RatingsCard.js
+++ src/data/RatingsCard.js
@@ -30,7 +30,8 @@ function RatingsCard() {
         imageSrc="https://images.unsplash.com/photo-1573496359142-b8d87734a5a2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fHByb2Zlc3Npb25hbCUyMHBvcnRyYWl0c3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
         authorName="Sarah Thompson"
       />
-    </div>
+         </div>
+   
   );
 }
 
